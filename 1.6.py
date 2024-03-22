54258b0c3f4a3f3108d5dcd58652239a832ec6b6
USER <ninamafra1212@gmail.com>
2024-03-21 21:23:44 -0500

    diccionario informacion_personal

---
+++ 1.6.py
@@ -0,0 +1,23 @@
+#diccionario informacion_personal
+
+diccionario={"nombre":"Eduardo","edad":"30","ciudad":"Quito","profecion":"Ingenerio"}
+
+#Acceder y Modificar Valores
+
+diccionario["ciudad"]="Cuenca"
+diccionario["profecion"]="Ingeniero"
+
+#Verificar Existencia de Claves
+
+diccionario["telefono"]="0986922436"
+
+#Eliminar una Clave
+del(diccionario["edad"])
+
+#Imprimir el Diccionario Final
+
+print(diccionario)
+
+
+
+
