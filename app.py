b046c0f4b429aa532916995a32579deef344ba9a
Nelsy <Nninamafra1212@gmail.com>
2025-08-21 19:15:13 -0500

    Primer: app Flask

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return '¡Hola, mundo! Esta es la ruta principal.'
+# Página de inicio
+@app.route("/")
+def index():
+    return render_template("index.html", title="Inicio")
 
-@app.route('/usuario/<nombre>')
-def usuario(nombre):
-    return f'Bienvenido, {nombre}!'
+# Página "Acerca de"
+@app.route("/about")
+def about():
+    return render_template("about.html", title="Acerca de")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
